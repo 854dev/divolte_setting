0531a3a5897357c35d14f82e37ae81d14eee619b
sangbong_dmk <sangbong@datamarketing.co.kr>
2022-04-26 14:19:19 +0900

    divolte 설정파일 업데이트

---
+++ mapping.groovy
@@ -0,0 +1,6 @@
+mapping {
+    // map content_id() onto 'content_id'
+    // map user_id() onto 'user_id'
+    // map stay_time() onto 'stay_time'
+    // map content_click_time() onto 'content_click_time'
+}
