b3c418642fa44caf12e38cd5d97de0ad6a3c95c2
sangbong_dmk <sangbong@datamarketing.co.kr>
2022-04-26 18:19:38 +0900

    스키마, 데이터 매핑파일 수정

--- mapping.groovy
+++ mapping.groovy
@@ -1,6 +1,36 @@
 mapping {
-    // map content_id() onto 'content_id'
-    // map user_id() onto 'user_id'
-    // map stay_time() onto 'stay_time'
-    // map content_click_time() onto 'content_click_time'
+    map duplicate() onto 'detectedDuplicate'
+    map corrupt() onto 'detectedCorruption'
+    map firstInSession() onto 'firstInSession'
+    map timestamp() onto 'timestamp'
+    map clientTimestamp() onto 'clientTimestamp'
+    map remoteHost() onto 'remoteHost'
+    map referer() onto 'referer'
+    map location() onto 'location'
+    map viewportPixelWidth() onto 'viewportPixelWidth'
+    map viewportPixelHeight() onto 'viewportPixelHeight'
+    map screenPixelWidth() onto 'screenPixelWidth'
+    map screenPixelHeight() onto 'screenPixelHeight'
+    map partyId() onto 'partyId'
+    map sessionId() onto 'sessionId'
+    map pageViewId() onto 'pageViewId'
+
+    map eventType() onto 'eventType'
+    map eventId() onto 'eventId'
+    map eventParameters().value('content_id') onto 'content_id'
+    map eventParameters().value('stay_time') onto 'stay_time'
+    map eventParameters().value('content_click_time') onto 'content_click_time'
+
+    def locationUri = parse location() to uri
+    map locationUri.path() onto 'uriPath'
+
+    def ua = userAgent()
+    map userAgentString() onto 'userAgentString'
+    map ua.name() onto 'userAgentName'
+    map ua.family() onto 'userAgentFamily'
+    map ua.vendor() onto 'userAgentVendor'
+    map ua.type() onto 'userAgentType'
+    map ua.osFamily() onto 'userAgentOsFamily'
+    map ua.osVersion() onto 'userAgentOsVersion'
+    map ua.osVendor() onto 'userAgentOsVendor'
 }
